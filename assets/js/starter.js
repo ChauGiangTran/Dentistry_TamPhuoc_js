6c6831484a45fcc85831ec9ecac86039b93b4b20
ChauGiangTran <tran.chaugiangk39@gmail.com>
2022-11-03 17:59:14 +0700

    add view

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -9,6 +9,7 @@
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/owl.carousel/dist/owl.carousel.js";
 
 
 // Option 2
@@ -20,3 +21,40 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // import "../../node_modules/bootstrap/js/dist/util.js";
 // import "../../node_modules/bootstrap/js/dist/modal.js";
+/**
+ * Owl Carousel
+ */
+var owl = $('.owl-carousel');
+var prevCarousel = `
+    <div class='nav-btn prev-slide rounded-circle'>
+        <svg class="bi text-white mr-2 pb-1" width="22" height="22">
+            <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#chevron-left" />
+        </svg></span>
+    </div>
+`
+var nextCarousel = `
+    <div class='nav-btn next-slide rounded-circle'>
+        <svg class="bi text-white mr-2 pb-1" width="22" height="22">
+            <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#chevron-right" />
+        </svg></span>
+    </div>
+`
+owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    },
+    navText: [prevCarousel, nextCarousel],
+})
